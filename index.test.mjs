d43ee236f09ab7667e61bd391275aa8f115203c8
Jacob Hummer <jcbhmr@outlook.com>
2023-06-08 21:50:04 +0000

    Initial thing

---
+++ index.test.mjs
@@ -0,0 +1,28 @@
+import test from "node:test";
+import assert from "node:assert";
+import { $ } from "zx";
+import * as index from "./index.js";
+
+const nativeNames = (
+  await $`NODE_NO_WARNINGS=1 node --expose-internals -r internal/test/binding -p 'Object.keys(primordials).join("\\n")'`
+)
+  .toString()
+  .trim()
+  .split("\n")
+  .sort();
+
+test("has all the right things on the default export", async () => {
+  for (const name of nativeNames) {
+    await test(`has ${name}`, () => {
+      assert(name in index.default, `missing ${name}!`);
+    });
+  }
+});
+
+// test("has the appropriate named exports", async () => {
+//   for (const name of nativeNames) {
+//     await test(`has ${name}`, () => {
+//       assert(name in index, `missing ${name}!`);
+//     });
+//   }
+// });
